e03464997bb9f770dcfc458820d98ddd5a8c9095
Selim <selimd3mirci@gmail.com>
2020-08-22 03:41:38 +0300

    ModelMapper added.

---
+++ src/main/java/com/demo/flightservice/utility/AdapterConf.java
@@ -0,0 +1,18 @@
+package com.demo.flightservice.utility;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdapterConf {
+
+    @Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+    }
+    
+}
